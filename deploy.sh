ca7dbde0eb352e14d67f93de9098a6e31d11a876
Anton Monakhov <a.monahov@tb-drone.ru>
2026-05-26 15:44:21 +0300

    deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+export ROS_DOMAIN_ID=1
+source /opt/ros/humble/setup.bash
+source sdk_deploy/install/setup.bash
+ros2 run m20_sdk_deploy rl_deploy
---
+++ simulate.sh
@@ -0,0 +1,3 @@
+export ROS_DOMAIN_ID=1
+source sdk_deploy/install/setup.bash
+python3 sdk_deploy/src/M20_sdk_deploy/interface/robot/simulation/mujoco_simulation_ros2.py
